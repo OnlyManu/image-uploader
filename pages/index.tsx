fcccc4b229682a54459a9d9e9d95747f85e5725a
emmanuel <blimanuevil@gmail.com>
2023-07-14 16:52:39 +0200

    initialisation du project

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Image uploader</title>
+        <meta name="description" content="Image uploader" />
+        <link rel="icon" href="/devchallenges.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
